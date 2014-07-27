172ea5611ca6f3ac0744435a79135a7a197c5662
Soumith Chintala <soumith@gmail.com>
2014-07-27 13:04:17 -0400

    error checking in pure cuda

--- test/test.lua
+++ test/test.lua
@@ -12,13 +12,10 @@ image = torch.randn(3, 128, 128, 128):cuda()
 output = model:forward(image)
 output2 = model2:forward(image)
 
-print("Max Error in outputs: " .. (output:float()-output2:float()):abs():max())
--- print((output:float()-output2:float()):abs():sum())
--- print(output[{{1,32},1,1,1}])
--- print(output2[32][1][1][1])
+print("Max Error in outputs: " .. (output:add(output2:mul(-1))):abs():max())
 
 cutorch:synchronize()
-runs = 1
+runs = 10
 
 local clk = os.clock()
 for i=1,runs do
