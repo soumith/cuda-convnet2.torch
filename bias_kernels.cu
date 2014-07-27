2c1f3051d1615ab48f19cced0ef12cff5028dacf
Soumith Chintala <soumith@gmail.com>
2014-07-27 13:03:56 -0400

    adding bias kernels

---
+++ bias_kernels.cu
@@ -0,0 +1,25 @@
+#include "nvmatrix/include/nvmatrix_kernels.cuh"
+#include "nvmatrix/include/nvmatrix_operators.cuh"
+#include <THC.h>
+#include <algorithm>
+#include "helper_cuda.h"
+
+extern "C" {
+  
+  void addBias(THCudaTensor* output, THCudaTensor* bias) {
+    int width = output->size[1];
+    int height = output->size[0];
+    float *odata = THCudaTensor_data(output);
+    float *bdata = THCudaTensor_data(bias);
+    dim3 threads(ADD_VEC_THREADS_X, ADD_VEC_THREADS_Y);
+    dim3 blocks(std::min(512, DIVUP(width, ADD_VEC_THREADS_X)),
+                std::min(NUM_BLOCKS_MAX, DIVUP(height, ADD_VEC_THREADS_Y)));
+    kColVectorOp<NVMatrixBinaryOps::Add>
+      <<<blocks, threads>>>(odata, bdata, odata, width, height, 
+                            output->stride[0], output->stride[0], 
+                            NVMatrixBinaryOps::Add());
+    getLastCudaError("Kernel execution failed");
+  }
+  
+
+}
