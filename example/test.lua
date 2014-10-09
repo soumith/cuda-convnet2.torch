afdb14a51eb5070fccd3d0b280476aa94452e62c
Sergey Zagoruyko <sergey.zagoruyko@imagine.enpc.fr>
2014-10-09 19:26:31 +0200

    added readme to the example

--- example/test.lua
+++ example/test.lua
@@ -1,30 +1,23 @@
 require 'image'
 require 'mattorch'
-dofile 'load.lua'
+dofile 'load_imagenet.lua'
 
-im2 = image.load('/opt/caffe/examples/images/cat.jpg')
-im3 = image.scale(im2,227,227,'bilinear')*256
-im4 = im3:clone()
-im4[{1,{},{}}] = im3[{3,{},{}}]
-im4[{3,{},{}}] = im3[{1,{},{}}]
+matfilename = arg[1]
+imagename = arg[2]
 
-img_mean = mattorch.load('./ilsvrc_2012_mean.mat')['img_mean']:transpose(3,1)
-im5 = im4 - image.scale(img_mean, 227, 227,'bilinear')
-I = im5
---im = mattorch.load('impros.mat')
+model = load_imagenet(matfilename)
+model:evaluate()
 
---I = im.image:transpose(2,3)
+im = image.load(imagename)
 
-bb = torch.Tensor(32,3,227,227)
+I = preprocess(im)
+batch = torch.Tensor(32,3,227,227)
 
 for i=1,32 do
-bb[i] = I
+  batch[i] = I
 end
 
-model:forward(bb:cuda())
+model:forward(batch:cuda())
 
 _,i = model:get(25).output[{1,{}}]:float():max(1)
 print('predicted class: ', i)
-
---conv5 = model.modules[15].output
---mattorch.save('conv5_rcnn.mat',{conv5_t = conv5:double()})
